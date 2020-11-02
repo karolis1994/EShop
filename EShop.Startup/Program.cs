36c743225b06a6e0f6fd239b31630f7a53a647d1
Karolis Pockevičius <karolispock@gmail.com>
2020-11-02 22:35:57 +0200

    startup project for migration

---
+++ EShop.Startup/Program.cs
@@ -0,0 +1,40 @@
+﻿using EShop.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
+
+namespace EShop.Startup
+{
+    class Program
+    {
+        public static IConfigurationRoot configuration;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(".");
+        }
+
+        public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<EShopContext>
+        {
+            public EShopContext CreateDbContext(string[] args)
+            {
+                var configuration = new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json")
+                        .Build();
+
+                var optionsBuilder = new DbContextOptionsBuilder<EShopContext>();
+
+                var connectionString = configuration
+                            .GetConnectionString("EShop");
+
+                optionsBuilder.UseSqlServer(connectionString);
+
+                return new EShopContext(optionsBuilder.Options);
+            }
+        }
+    }
+}
